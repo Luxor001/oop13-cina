007aca379f1e97f6ad0dfb366dd19b21cafe4cda
Francesco Cozzolino <Francesco Cozzolino@localhost>
2014-02-14 22:40:00 +0100

    first proof of file sending (perfectly working). you can find the code in the client and server classes in the form of comments

--- client_chat/src/client_chat/Client.java
+++ client_chat/src/client_chat/Client.java
@@ -1,6 +1,7 @@
 package client_chat;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -27,181 +28,234 @@ import javax.swing.JTextArea;
  dal programmatore*/
 
 public class Client implements Runnable {
-	private SSLSocketFactory sslSocketFactory = null;
-	private SSLSocket sslSocket = null;
-	private ObjectOutputStream oos = null;
-	private ObjectInputStream ois = null;
-	private String str = "";
-	private JTextArea chat;
-	private String ip;
-	private String nameClient = System.getProperty("user.name");
-	private String nameServer = null;
-	private boolean resetTime = false;
-	private boolean stop = false;
-	private CountDownLatch latch = new CountDownLatch(1);
+    private SSLSocketFactory sslSocketFactory = null;
+    private SSLSocket sslSocket = null;
+    private ObjectOutputStream oos = null;
+    private ObjectInputStream ois = null;
+    private String str = "";
+    private JTextArea chat;
+    private String ip;
+    private String nameClient = System.getProperty("user.name");
+    private String nameServer = null;
+    private boolean resetTime = false;
+    private boolean stop = false;
+    private CountDownLatch latch = new CountDownLatch(1);
 
-	public Client(String ip, JTextArea chat) throws IOException,
-			ClassNotFoundException {
+    FileOutputStream outStream = new FileOutputStream(
+	    "C:\\Users\\Francesco\\Desktop\\Radioactive.mp3");
+    byte[] buffer = new byte[200000];
+    int bytesRead = 0, counter = 0;
+    Object o = "test";
 
-		String path = "ClientKey.jks";
-		char[] passphrase = "changeit".toCharArray();
-		KeyStore keystore;
-		TrustManagerFactory tmf;
-		SSLContext context;
-		TrustManager[] trustManagers;
+    public Client(String ip, JTextArea chat) throws IOException,
+	    ClassNotFoundException {
 
-		this.ip = ip;
-		this.chat = chat;
+	String path = "ClientKey.jks";
+	char[] passphrase = "changeit".toCharArray();
+	KeyStore keystore;
+	TrustManagerFactory tmf;
+	SSLContext context;
+	TrustManager[] trustManagers;
 
-		try {
-			// indico il tipo della chiave
-			keystore = KeyStore.getInstance("jks");
-			// carico la chiave
-			keystore.load(new FileInputStream(path), null);
+	this.ip = ip;
+	this.chat = chat;
 
-			// creo un'istanza che utilizza l'algoritmo "sunx509"
-			KeyManagerFactory clientKeyManager = KeyManagerFactory
-					.getInstance("SunX509");
-			clientKeyManager.init(keystore, passphrase);
+	try {
+	    // indico il tipo della chiave
+	    keystore = KeyStore.getInstance("jks");
+	    // carico la chiave
+	    keystore.load(new FileInputStream(path), null);
 
-			// ottengo la chiave pubblica
-			KeyStore serverPub = KeyStore.getInstance("jks");
-			// successivamente verr� inviata dal webserver
+	    // creo un'istanza che utilizza l'algoritmo "sunx509"
+	    KeyManagerFactory clientKeyManager = KeyManagerFactory
+		    .getInstance("SunX509");
+	    clientKeyManager.init(keystore, passphrase);
 
-			serverPub.load(new FileInputStream("ServerKey.jks"), null);
+	    // ottengo la chiave pubblica
+	    KeyStore serverPub = KeyStore.getInstance("jks");
+	    // successivamente verr� inviata dal webserver
 
-			tmf = TrustManagerFactory.getInstance("SunX509");
-			tmf.init(serverPub);
+	    serverPub.load(new FileInputStream("ServerKey.jks"), null);
 
-			context = SSLContext.getInstance("SSL");
-			trustManagers = tmf.getTrustManagers();
-			context.init(clientKeyManager.getKeyManagers(), trustManagers,
-					SecureRandom.getInstance("SHA1PRNG"));
-			sslSocketFactory = context.getSocketFactory();
-		} catch (KeyStoreException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (CertificateException e) {
-			e.printStackTrace();
-		} catch (KeyManagementException e) {
-			e.printStackTrace();
-		} catch (UnrecoverableKeyException e) {
-			e.printStackTrace();
-		}
+	    tmf = TrustManagerFactory.getInstance("SunX509");
+	    tmf.init(serverPub);
 
-		new Thread(this).start();
-
+	    context = SSLContext.getInstance("SSL");
+	    trustManagers = tmf.getTrustManagers();
+	    context.init(clientKeyManager.getKeyManagers(), trustManagers,
+		    SecureRandom.getInstance("SHA1PRNG"));
+	    sslSocketFactory = context.getSocketFactory();
+	} catch (KeyStoreException e) {
+	    e.printStackTrace();
+	} catch (NoSuchAlgorithmException e) {
+	    e.printStackTrace();
+	} catch (CertificateException e) {
+	    e.printStackTrace();
+	} catch (KeyManagementException e) {
+	    e.printStackTrace();
+	} catch (UnrecoverableKeyException e) {
+	    e.printStackTrace();
 	}
 
-	public void run() {
+	new Thread(this).start();
 
-		// stabilisco la connessione con il server
-		try {
-			sslSocket = (SSLSocket) sslSocketFactory.createSocket(ip, 9999);
-			sslSocket.startHandshake();
-		} catch (UnknownHostException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+    }
 
-		System.out.println("** Sono connesso con il server **");
-		System.out.println("IP: " + sslSocket.getInetAddress());
-		System.out.println("Porta: " + sslSocket.getPort());
+    public void run() {
 
-		// inizializzo gli stream che mi permetteranno di inviare e ricevere i
-		// mess ->
+	// stabilisco la connessione con il server
+	try {
+	    sslSocket = (SSLSocket) sslSocketFactory.createSocket(ip, 9999);
+	    sslSocket.startHandshake();
+	} catch (UnknownHostException e1) {
+	    e1.printStackTrace();
+	} catch (IOException e1) {
+	    e1.printStackTrace();
+	}
 
-		try {
-			oos = new ObjectOutputStream(sslSocket.getOutputStream());
-			ois = new ObjectInputStream(sslSocket.getInputStream());
+	System.out.println("** Sono connesso con il server **");
+	System.out.println("IP: " + sslSocket.getInetAddress());
+	System.out.println("Porta: " + sslSocket.getPort());
 
-			oos.writeObject(nameClient);
-			oos.flush();
-			nameServer = (String) ois.readObject();
-		} catch (IOException | ClassNotFoundException e1) {
-			e1.printStackTrace();
-		}
+	// inizializzo gli stream che mi permetteranno di inviare e ricevere i
+	// mess ->
 
-		class Timer extends Thread {
-			private final static int TIMEOUT = 60000;
-			private int sleep = 1000;
-			private int currentTime = 0;
-			private boolean timeout = false;
+	try {
+	    oos = new ObjectOutputStream(sslSocket.getOutputStream());
+	    ois = new ObjectInputStream(sslSocket.getInputStream());
 
-			public void run() {
-				while (!timeout) {
-					try {
-						Thread.sleep(sleep);
-					} catch (InterruptedException e) {
-					}
+	    oos.writeObject(nameClient);
+	    oos.flush();
+	    //SEND FILE
+	    //ois.readObject();
+	    //SEND FILE
+	    nameServer = (String) ois.readObject();
+	} catch (IOException | ClassNotFoundException e1) {
+	    e1.printStackTrace();
+	}
 
-					currentTime += sleep;
+	class Timer extends Thread {
+	    private final static int TIMEOUT = 60000;
+	    private int sleep = 1000;
+	    private int currentTime = 0;
+	    private boolean timeout = false;
 
-					if (resetTime) {
-						resetTime = false;
-						currentTime = 0;
-					}
-					if (currentTime > TIMEOUT || stop) {
-						sendMessage(null);
-						timeout = true;
-					}
-				}
-			}
+	    public void run() {
+		while (!timeout) {
+		    try {
+			Thread.sleep(sleep);
+		    } catch (InterruptedException e) {
+		    }
 
-			public boolean getStateTimeout() {
-				return timeout;
-			}
-		}
+		    currentTime += sleep;
 
-		Timer t = new Timer();
-		t.start();
-
-		// leggo quello che mi arriva dal server
-		try {
-			while ((str = (String) ois.readObject()) != null) {
-
-				chat.append(nameServer + " : " + str + "\n");
-				resetTime = true;
-			}
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		if (!t.getStateTimeout()) {
-			t.interrupt();
+		    if (resetTime) {
+			resetTime = false;
+			currentTime = 0;
+		    }
+		    if (currentTime > TIMEOUT || stop) {
 			sendMessage(null);
+			timeout = true;
+		    }
 		}
-		try {
-			oos.close();
-			ois.close();
-			sslSocket.close();
-			latch.countDown();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    }
+
+	    public boolean getStateTimeout() {
+		return timeout;
+	    }
 	}
 
-	public synchronized void sendMessage(String message) {
-		if (sslSocket.isConnected() && !sslSocket.isClosed()) {
-			try {
-				resetTime = true;
-				oos.writeObject(message);
-				oos.flush();
-			} catch (IOException e) {
-			}
-		}
+	Timer t = new Timer();
+	t.start();
 
+	// leggo quello che mi arriva dal server
+	try {
+
+	    //LOGOUT
+	    while ((str = (String) ois.readObject()) != null) {
+		chat.append(nameServer + " : " + str + "\n");
+		resetTime = true;
+	    }
+	    //LOGOUT
+
+	    /*   //SEND FILE
+	       int oldstep = 0;
+	       boolean isFile = false;
+
+	       while ((o = ois.readObject()) != null) {
+
+	    if (o instanceof Boolean) {
+	        if ((boolean) o) {
+	    	int step = ois.readInt();
+
+	    	bytesRead = step;
+	    	ois.readFully(buffer, 0, step);
+
+	    	//bytesRead = ois.read(buffer, 0, step);
+	    	if (bytesRead >= 0) {
+
+	    	    outStream.write(buffer, 0, bytesRead);
+
+	    	    counter += bytesRead;
+	    	    System.out.println("total bytes read: " + counter);
+	    	}
+	    	if (bytesRead < 1024) {
+	    	    outStream.flush();
+	    	    outStream.close();
+	    	}
+	    	oldstep = bytesRead;
+	        } else {
+	    	chat.append(nameServer + " : "
+	    		+ (String) ois.readObject() + "\n");
+	        }
+	    }
+	       }*/
+	    //SEND FILE
+	} catch (IOException | ClassNotFoundException e) {
+	    //SEND FILE
+	    /*  System.out.println(bytesRead);
+	      try {
+	    outStream.close();
+	      } catch (IOException e1) {
+	    e1.printStackTrace();
+	      }*/
+	    //SEND FILE
+	    e.printStackTrace();
 	}
 
-	public void close() {
-		stop = true;
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	if (!t.getStateTimeout()) {
+	    t.interrupt();
+	    sendMessage(null);
 	}
+	try {
+	    oos.close();
+	    ois.close();
+	    sslSocket.close();
+	    latch.countDown();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public synchronized void sendMessage(String message) {
+	if (sslSocket.isConnected() && !sslSocket.isClosed()) {
+	    try {
+		resetTime = true;
+		oos.writeObject(message);
+		oos.flush();
+	    } catch (IOException e) {
+	    }
+	}
+
+    }
+
+    public void close() {
+	stop = true;
+	try {
+	    latch.await();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
 
 }
--- client_chat/src/client_chat/Server.java
+++ client_chat/src/client_chat/Server.java
@@ -17,181 +17,232 @@ import javax.net.ssl.SSLSocket;
 
 public class Server implements Runnable {
 
-	private SSLServerSocket sslServerSocket;
-	private SSLServerSocketFactory sslServerSocketFactory = null;
-	// list of clients connected to server
-	private List<MessageFromClient> client = new ArrayList<>();
-	private ViewObserver controller;
+    private SSLServerSocket sslServerSocket;
+    private SSLServerSocketFactory sslServerSocketFactory = null;
+    // list of clients connected to server
+    private List<MessageFromClient> client = new ArrayList<>();
+    private ViewObserver controller;
 
-	public Server(ViewObserver controller) throws IOException,
-			ClassNotFoundException {
+    public Server(ViewObserver controller) throws IOException,
+	    ClassNotFoundException {
 
-		this.controller = controller;
+	this.controller = controller;
 
-		try {
+	try {
 
-			KeyStore serverKeys = KeyStore.getInstance("JKS");
-			serverKeys.load(new FileInputStream("ServerKey.jks"), null);
+	    KeyStore serverKeys = KeyStore.getInstance("JKS");
+	    serverKeys.load(new FileInputStream("ServerKey.jks"), null);
 
-			KeyManagerFactory serverKeyManager = KeyManagerFactory
-					.getInstance("SunX509");
+	    KeyManagerFactory serverKeyManager = KeyManagerFactory
+		    .getInstance("SunX509");
 
-			serverKeyManager.init(serverKeys, "password".toCharArray());
+	    serverKeyManager.init(serverKeys, "password".toCharArray());
 
-			// creo il socket utilizzando il protocollo SSl
-			SSLContext ssl = SSLContext.getInstance("SSL");
-			ssl.init(serverKeyManager.getKeyManagers(), null,
-					SecureRandom.getInstance("SHA1PRNG"));
+	    // creo il socket utilizzando il protocollo SSl
+	    SSLContext ssl = SSLContext.getInstance("SSL");
+	    ssl.init(serverKeyManager.getKeyManagers(), null,
+		    SecureRandom.getInstance("SHA1PRNG"));
 
-			sslServerSocketFactory = ssl.getServerSocketFactory();
+	    sslServerSocketFactory = ssl.getServerSocketFactory();
 
-		} catch (Exception e) {
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	// creo il server
+	sslServerSocket = (SSLServerSocket) sslServerSocketFactory
+		.createServerSocket(9999);
+	sslServerSocket.setNeedClientAuth(false);
+	new Thread(this).start();
+
+    }
+
+    public void run() {
+
+	System.out.println("In attesa di client...");
+
+	while (true) {
+	    try {
+
+		client.add(new MessageFromClient((SSLSocket) sslServerSocket
+			.accept(), controller));
+
+		client.get(client.size() - 1).start();
+
+		//SEND FILE
+		/*		client.get(client.size() - 1).sendMessage("Paperino");
+				File file = new File(
+					"F:\\Radioactive.mp3");
+				long fileSize = file.length();
+				long completed = 0;
+				int step = 150000;
+				long size = fileSize;
+				System.out.println("File sending");
+				FileInputStream fileStream = new FileInputStream(file);
+				byte[] buffer = new byte[step];
+				while (completed < fileSize) {
+				    size -= step;
+				    int oldstep=step;
+				    if (size < 0) {
+					step = (int) (size + step);
+				    }
+				    fileStream.read(buffer);
+				    client.get(client.size() - 1).sendMessage(buffer, step);
+				    completed += oldstep;
+				}
+				
+				fileStream.close();
+				System.out.println("File Send");
+		*/
+		//SEND FILE
+	    } catch (IOException e1) {
+		e1.printStackTrace();
+	    }
+	}
+
+    }
+
+    public boolean sendMessage(String message, String name) {
+
+	for (int i = 0; i < client.size(); i++) {
+	    if (client.get(i).getNameClient().equals(name)) {
+		if (!client.get(i).isClosed() && client.get(i).isConnected()) {
+		    try {
+			client.get(i).sendMessage(message);
+			return true;
+		    } catch (IOException e) {
 			e.printStackTrace();
+		    }
+		} else {
+		    client.remove(i);
 		}
-		// creo il server
-		sslServerSocket = (SSLServerSocket) sslServerSocketFactory
-				.createServerSocket(9999);
-		sslServerSocket.setNeedClientAuth(false);
-		new Thread(this).start();
 
+	    }
+	}
+
+	return false;
+    }
+
+    public void close() {
+	while (client.size() > 0) {
+	    if (!client.get(client.size() - 1).isClosed()
+		    && client.get(client.size() - 1).isConnected()) {
+		try {
+		    client.get(client.size() - 1).sendMessage(null);
+		    client.remove(client.size() - 1);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+
+	    } else {
+		client.remove(client.size() - 1);
+	    }
+	}
+    }
+
+    private static class MessageFromClient extends Thread {
+	private ViewObserver controller;
+	private ObjectInputStream ois = null;
+	private ObjectOutputStream oos = null;
+	private SSLSocket sslSocket;
+	private String str = null;
+	private String nameServer = System.getProperty("user.name");
+	private String nameClient = null;
+	private boolean close = false;
+
+	public MessageFromClient(SSLSocket sslSocket, ViewObserver controller) {
+	    this.controller = controller;
+	    this.sslSocket = sslSocket;
+	    try {
+		ois = new ObjectInputStream(sslSocket.getInputStream());
+		oos = new ObjectOutputStream(sslSocket.getOutputStream());
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
 	}
 
 	public void run() {
 
-		System.out.println("In attesa di client...");
+	    System.out.println("** Un client si è connesso **");
+	    System.out.println("IP: " + sslSocket.getInetAddress());
+	    System.out.println("Porta: " + sslSocket.getPort());
 
-		while (true) {
-			try {
+	    try {
+		//LOGOUT
+		oos.writeObject(nameServer);
+		oos.flush();
+		//LOGUT
+		nameClient = (String) ois.readObject();
+	    } catch (IOException | ClassNotFoundException e1) {
+		e1.printStackTrace();
+	    }
 
-				client.add(new MessageFromClient((SSLSocket) sslServerSocket
-						.accept(), controller));
+	    // leggo quello che mi arriva dal client
+	    try {
 
-				client.get(client.size() - 1).start();
-
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
+		while ((str = (String) ois.readObject()) != null) {
+		    controller.commandReceiveMessage(nameClient + " : " + str,
+			    nameClient);
 		}
+
+		if (!close) {
+		    sendMessage(null);
+		}
+		oos.close();
+		ois.close();
+		sslSocket.close();
+	    } catch (IOException | ClassNotFoundException e) {
+		e.printStackTrace();
+	    }
 
 	}
 
-	public boolean sendMessage(String message, String name) {
+	public synchronized void sendMessage(Object message) throws IOException {
 
-		for (int i = 0; i < client.size(); i++) {
-			if (client.get(i).getNameClient().equals(name)) {
-				if (!client.get(i).isClosed() && client.get(i).isConnected()) {
-					try {
-						client.get(i).sendMessage(message);
-						return true;
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				} else {
-					client.remove(i);
-				}
+	    if (!close) {
 
-			}
-		}
+		//SEND FILE
+		/*oos.writeObject(false);
+		oos.flush();*/
+		//SEND FILE
+		oos.writeObject(message);
+		oos.flush();
+	    }
 
-		return false;
+	    if (message == null) {
+		close = true;
+	    }
 	}
 
-	public void close() {
-		while (client.size() > 0) {
-			if (!client.get(client.size() - 1).isClosed()
-					&& client.get(client.size() - 1).isConnected()) {
-				try {
-					client.get(client.size() - 1).sendMessage(null);
-					client.remove(client.size() - 1);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+	public synchronized void sendMessage(byte[] message, int step)
+		throws IOException {
 
-			} else {
-				client.remove(client.size() - 1);
-			}
-		}
+	    if (!close) {
+
+		oos.writeObject(true);
+		oos.flush();
+		oos.writeInt(step);
+		oos.flush();
+		oos.write(message, 0, 150000);
+		oos.flush();
+	    }
+
+	    if (message == null) {
+		close = true;
+	    }
 	}
 
-	private static class MessageFromClient extends Thread {
-		private ViewObserver controller;
-		private ObjectInputStream ois = null;
-		private ObjectOutputStream oos = null;
-		private SSLSocket sslSocket;
-		private String str = null;
-		private String nameServer = System.getProperty("user.name");
-		private String nameClient = null;
-		private boolean close = false;
-
-		public MessageFromClient(SSLSocket sslSocket, ViewObserver controller) {
-			this.controller = controller;
-			this.sslSocket = sslSocket;
-			try {
-				ois = new ObjectInputStream(sslSocket.getInputStream());
-				oos = new ObjectOutputStream(sslSocket.getOutputStream());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		public void run() {
-
-			System.out.println("** Un client si è connesso **");
-			System.out.println("IP: " + sslSocket.getInetAddress());
-			System.out.println("Porta: " + sslSocket.getPort());
-
-			try {
-				oos.writeObject(nameServer);
-				oos.flush();
-				nameClient = (String) ois.readObject();
-			} catch (IOException | ClassNotFoundException e1) {
-				e1.printStackTrace();
-			}
-
-			// leggo quello che mi arriva dal client
-			try {
-
-				while ((str = (String) ois.readObject()) != null) {
-					controller.commandReceiveMessage(nameClient + " : " + str,
-							nameClient);
-				}
-
-				if (!close) {
-					sendMessage(null);
-				}
-				oos.close();
-				ois.close();
-				sslSocket.close();
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-		public synchronized void sendMessage(String message) throws IOException {
-
-			if (!close) {
-				oos.writeObject(message);
-				oos.flush();
-			}
-
-			if (message == null) {
-				close = true;
-			}
-		}
-
-		public String getNameClient() {
-			return nameClient;
-		}
-
-		public boolean isConnected() {
-			return sslSocket.isConnected();
-		}
-
-		public boolean isClosed() {
-			return sslSocket.isClosed();
-		}
-
+	public String getNameClient() {
+	    return nameClient;
 	}
+
+	public boolean isConnected() {
+	    return sslSocket.isConnected();
+	}
+
+	public boolean isClosed() {
+	    return sslSocket.isClosed();
+	}
+
+    }
 
 }
