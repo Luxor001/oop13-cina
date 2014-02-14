c8d6784d7f25c1a741d83eeffd1b7088ab17369e
Francesco Cozzolino <Francesco Cozzolino@localhost>
2014-02-14 22:40:00 +0100

    first proof of file sending (perfectly working). you can find the code in the client and server classes in the form of comments

--- client_chat/src/client_chat/Application.java
+++ client_chat/src/client_chat/Application.java
@@ -2,42 +2,39 @@ package client_chat;
 
 import java.io.IOException;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
 import client_chat.Model.connectionResult;
 
 public class Application {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		new SplashScreen();
-		/*
-		
-		Controller c = new Controller();
-		View v = new View();
-		Model m = new Model();
-		c.setView(v);
-		c.setModel(m);
+	//new SplashScreen();
 
-		WebsocketHandler.setController(c);
-		connectionResult result;
-		int userchoice = 0;
-		result = m.AttemptConnection();
-		/* connects to webserver 
+	Controller c = new Controller();
+	View v = new View();
+	Model m = new Model();
+	c.setView(v);
+	c.setModel(m);
 
-		while (result == connectionResult.TIMEOUT && userchoice == 0) {
+	WebsocketHandler.setController(c);
+	connectionResult result;
+	int userchoice = 0;
+	result = m.AttemptConnection();
 
-			userchoice = c.buildChoiceMessageBox(
-					"Chat Channel is not responding," + "\nconnection failed",
-					"Connection Failed", new Object[] { "Reconnect",
-							"Quit to Main" }, JOptionPane.ERROR_MESSAGE);
-			if (userchoice == 0) /* retry 
-				result = m.AttemptConnection();
-			if (userchoice == 1)
-				;
-			/* splashscreen, needs to be implemented 
+	// connects to webserver 
+
+	/*	while (result == connectionResult.TIMEOUT && userchoice == 0) {
+
+		    userchoice = c.buildChoiceMessageBox(
+			    "Chat Channel is not responding," + "\nconnection failed",
+			    "Connection Failed", new Object[] { "Reconnect",
+				    "Quit to Main" }, JOptionPane.ERROR_MESSAGE);
+		    if (userchoice == 0) // retry 
+			result = m.AttemptConnection();
+		    if (userchoice == 1)
+			;
+		  //   splashscreen, needs to be implemented 
 		}
-*/
-	}
+	*/
+    }
 }
