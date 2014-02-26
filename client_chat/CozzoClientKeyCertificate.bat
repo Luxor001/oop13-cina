8ead31e95c486f60626b491b00732d406f943c6c
FrancescoCozzolino <FrancescoCozzolino@localhost>
2014-02-26 11:35:28 +0100

    i forgot to modified some lines of code

--- client_chat/CozzoClientKeyCertificate.bat
+++ client_chat/CozzoClientKeyCertificate.bat
@@ -1,3 +1,3 @@
 cd E:\java\jdk\jre
-(echo cozzo & echo cozzo & echo cozzo & echo & echo  & echo  & echo si) | keytool -genkey -alias ClientKey -keyalg RSA -keypass changeit -storepass changeit -keystore E:\java\workspace\client_chat/cozzoClientKey.jks
-keytool -export -alias ClientKey -storepass changeit -file E:\java\workspace\client_chat/cozzoClientKeyCertificate.cer -keystore E:\java\workspace\client_chat/cozzoClientKeyKey.jks
+(echo Cozzo & echo Cozzo & echo Cozzo & echo & echo  & echo  & echo si) | keytool -genkey -alias ClientKey -keyalg RSA -keypass changeit -storepass changeit -keystore E:\java\workspace\client_chat/CozzoClientKey.jks
+keytool -export -alias ClientKey -storepass changeit -file E:\java\workspace\client_chat/CozzoClientKeyCertificate.cer -keystore E:\java\workspace\client_chat/CozzoClientKeyKey.jks
