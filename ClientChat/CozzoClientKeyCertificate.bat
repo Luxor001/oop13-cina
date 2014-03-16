13dcff541ef3b29c108b3930ec5c5253a1f744dd
Luxor002 <Luxor002@localhost>
2014-03-16 23:23:11 +0100

    Deleted some useless files(again).

--- ClientChat/CozzoClientKeyCertificate.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Program Files\Java\jre7
-(echo Cozzo & echo Cozzo & echo Cozzo & echo  & echo  & echo  & echo si) | keytool -genkey -alias ClientKey -keyalg RSA -keypass IjyLDTgy -storepass IjyLDTgy -keystore C:\Users\Francesco\Desktop\workspace\ClientChat/CozzoClientKey.jks
-keytool -export -alias ClientKey -storepass IjyLDTgy -file C:\Users\Francesco\Desktop\workspace\ClientChat/CozzoClientKeyCertificate.cer -keystore C:\Users\Francesco\Desktop\workspace\ClientChat/CozzoClientKeyKey.jks
--- ClientChat/CozzoServerKeyCertificate.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Program Files\Java\jre7
-(echo Cozzo & echo Cozzo & echo Cozzo & echo  & echo  & echo  & echo si) | keytool -genkey -alias ServerKey -keyalg RSA -keypass myOU@jxZ -storepass myOU@jxZ -keystore C:\Users\Francesco\Desktop\workspace\ClientChat/CozzoServerKey.jks
-keytool -export -alias ServerKey -storepass myOU@jxZ -file C:\Users\Francesco\Desktop\workspace\ClientChat/CozzoServerKeyCertificate.cer -keystore C:\Users\Francesco\Desktop\workspace\ClientChat/CozzoServerKeyKey.jks
